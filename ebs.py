ef74f2a1ec5646ba87e611cb3bc36c20b1c0c274
sravanth2002 <19bd1a12a9@gmail.com>
2024-11-08 20:00:42 +0530

    ADDED LOG FOR EBS

--- ebs.py
+++ ebs.py
@@ -5,6 +5,8 @@ def run_ebs():
     ec2_client = boto3.client('ec2')
     regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
     
+    data = []
+    
     for region in regions:
         ec2 = boto3.resource('ec2', region_name=region)
         volumes = ec2.volumes.filter(Filters=[{'Name': 'status', 'Values': ['available']}])
@@ -20,6 +22,20 @@ def run_ebs():
                 snapshot = volume.create_snapshot(Description=f"Snapshot of {volume_id} before deletion")
                 print(f"Created snapshot {snapshot.snapshot_id} for volume {volume_id}")
                 
+                data.append([region,volume_id,volume.state, len(attachments),snapshot.snapshot_id])
+
                 # Delete the volume
-                volume.delete()
+                # volume.delete()
+
                 print(f"Deleted volume {volume_id}")
+    headers = ["Region","Volume ID","State", "Attachments", "Snapshot ID"]
+    table = f"{headers[0]:<15} | {headers[1]:<20} | {headers[2]:<10} | {headers[3]:<12} | {headers[4]:<20}\n"
+    table += "-" * 80 + "\n"
+    for row in data:
+        table += f"{row[0]:<15} | {row[1]:<20} | {row[2]:<10} | {row[3]:<12} | {row[4]:<20}\n"
+    print(table)
+
+    # Append the table to the log file
+    with open('/tmp/details.log','a') as log_file:
+        log_file.write("\n\nEBS Volumes with no attachments:\n")
+        log_file.write(table)
